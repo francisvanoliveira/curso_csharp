173ec2701e8c1c27865832acda7f70d305fe9b71
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2022-12-29 02:00:18 -0400

    Saída de dados
    Aula referente a asida de dados

---
+++ Saida/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Exemplos para saída de dados Aula C#*/
+
+using System;
+using System.Globalization; /*para poder usar o CultureInfo.InvariantCulture*/
+
+namespace Saida{
+    class Program{
+        static void Main(string[] args){
+
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom dia"); /*não tem quebra de linha*/
+            Console.WriteLine("Boa tarde"); /*com quebra de linha*/
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2")); /*para mostrar duas casas decimais apos a virgula*/
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); /*mostra o ponto em vez da virugula*/
+            Console.WriteLine(nome);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Exemplo com Placeholder");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Exemplo com Interpolacao");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Exemplo com Concatenacao");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais.");
+
+        }
+    }
+}
---
+++ SaidaExercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaExercicio{
+    class Program{
+        static void Main(string[] args){
+
+            string prod1 = "Computador";
+            string prod2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int cod = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.Write("Produto:");
+            Console.WriteLine($"{prod1}, cujo preço é ${preco1:F2}");
+            Console.WriteLine($"{prod2}, cujo preço é ${preco2:F2}");
+            Console.WriteLine(" ");
+            Console.WriteLine("Registro: " + idade + " anos de idade, código " + cod + " e gênero: " + genero);
+            Console.WriteLine(" ");
+            Console.WriteLine("Medida com oito casas decimais: " + medida.ToString("F8"));
+            Console.WriteLine("Arredondado com 3 casas decimais: " + medida.ToString("F3"));
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
