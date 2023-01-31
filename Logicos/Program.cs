f08a7cb627ab21575f0f11fe71113a2d688d7ca6
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2023-01-30 20:10:43 -0400

    Logicos
    Aula de operadores lógicos

---
+++ Logicos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Logico{
+    class Program{
+        static void Main(string[] args){
+            
+            //operado logico and
+            bool c1 = 2 > 3 && 4 != 5;
+            Console.WriteLine(c1);
+
+            //operado logico or
+            bool c2 = 2 > 3 || 4 != 5;
+            Console.WriteLine(c2);
+
+            //operado logico no
+            bool c3 = !(2 > 3) && 4 != 5;
+            Console.WriteLine(c3);
+        }
+    }
+}
