9954fb306e559834b46351f8782cb6b08145dfba
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2023-01-30 23:40:14 -0400

    Condicional
    Aula estrutura condicional if-else

---
+++ Condicional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Condicional{
+    class Program{
+        static void Main(string[] args){
+            
+            //Composta
+            Console.WriteLine("Entre com um número inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if(x % 2 == 0){
+                Console.WriteLine("Par!");
+            }
+            else {
+                Console.WriteLine("Impar");
+            }
+
+            //Encadeamentos
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if(hora < 12){
+                Console.WriteLine("Bom dia");
+            }
+            else if(hora >= 12 && hora < 18){
+                Console.WriteLine("Boa tarde");
+            }
+            else{
+                Console.WriteLine("Boa noite");
+            }
+        }
+    }
+}
